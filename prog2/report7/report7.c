2ebdeaa21a2eae6fcb347853017baa841824616f
Maeshiro Takao <e225757@ie.u-ryukyu.ac.jp>
2023-02-14 14:15:36 +0900

    report7

---
+++ prog2/report7/report7.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+void sort(int scores[], int size){
+    int temp; //値入れ替え用の一時変数
+    int i, j;
+
+    printf("scores = ");
+    for (i = 0; i<size; i++) {
+        printf("%d,",scores[i]);    
+    }
+
+    for (i=0; i<size; ++i) {
+        for (j=i+1; j<size; ++j) {
+
+            if (scores[i] < scores[j]) {
+                temp =  scores[i];
+                scores[i] = scores[j];
+                scores[j] = temp;
+            }
+        }
+    }
+
+    printf("\n");
+    printf("result = ");
+    for (i = 0; i<size; i++) {
+        printf("%d,",scores[i]);    
+    }
+}
+
+
+int main(){
+    const int size = 6;
+    int scores[size] = {0, 60, 70, 100, 68, 50};
+
+    sort(scores, size);
+    
+    return 0;
+}
